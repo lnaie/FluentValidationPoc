67d58abf33db41b6350deeeead6ff3bd50883b18
Lucian Naie <lucian.naie@t-mobile.nl>
2020-11-10 15:33:10 +0100

    POC for an intriguing behaviour of fluent validation on top of asp.net core 3.1

---
+++ FluentValidationPoc/Controllers/Content/Models/PutAssetRequestModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace FluentValidationPoc.Controllers.Content.Models
+{
+    public class PutAssetRequestModel
+    {
+        public string AssetId { get; set; }
+        public string ProviderId { get; set; }
+        public string Provider { get; set; }
+        public int? VersionMajor { get; set; }
+        public int? VersionMinor { get; set; }
+        public DateTime? CreationDate { get; set; }
+    }
+}
---
+++ FluentValidationPoc/Controllers/Content/Models/ReleaseRequestModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace FluentValidationPoc.Controllers.Content.Models
+{
+    public class ReleaseRequestModel
+    {
+        public string ProviderId { get; set; }
+        public string ReleaseAssetId { get; set; }
+    }
+}
